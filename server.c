d57dac6098a23a4a2bcadfbdb1cac63ab470136e
Zephyr <3150105024@zju.edu.cn>
2018-04-08 16:16:38 +0800

    multithread added

--- server.c
+++ server.c
@@ -1,12 +1,27 @@
 #include "server_util.h"
 
+int server_should_stop = 0;
+
+void* start_serve(void* client_desc) 
+{
+    service(*(int*)client_desc);
+    pthread_exit(0);
+}
+
 int main(int argc, char **argv)
 {
     int server_desc = new_socket(8888);
     if(server_desc < 0) return server_desc;
-    int client_desc;
-    int accept_exitcode = accept_client(server_desc, &client_desc);
-    if(accept_exitcode != SUCCEED_EXITCODE) return accept_exitcode;
-    service(client_desc);
+    while (!server_should_stop)
+    {
+        int client_desc;
+        int accept_exitcode = accept_client(server_desc, &client_desc);
+        if(accept_exitcode != SUCCEED_EXITCODE) return accept_exitcode;
+
+        pthread_t  pid;
+        pthread_create(&pid, NULL, start_serve, &client_desc);
+
+        //service(client_desc);
+    }
     return 0;
 }
