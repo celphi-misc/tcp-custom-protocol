de7b2c13bae43238a64e44521e7fbfdb48993386
Zephyr <3150105024@zju.edu.cn>
2018-04-08 16:30:03 +0800

    more interaction

--- client_cli.c
+++ client_cli.c
@@ -1,10 +1,29 @@
 #include "client_cli.h"
+#define CLI_TEST
 
 char *s;
+int io_lock;
+
+void io_locked() 
+{
+    io_lock = 1;
+}
+
+void io_release() 
+{
+    io_lock = 0;
+}
+
+int io_should_wait() 
+{
+    if (io_lock == 1) return 1;
+    else return 0;
+}
 
 void cli_init()
 {
     s = (char*)malloc(sizeof(char)*256);
+    io_lock = 0;
 }
 
 int show_welcome() 
@@ -35,7 +54,8 @@ int process_input_menu()
     if(strcmp(s, "connect") ==0 || strcmp(s, "c")==0) return INPUT_CONNECT;
     else if(strcmp(s, "help") == 0 || strcmp(s, "h") == 0) return show_help();
     else if(strcmp(s, "exit") ==0 || strcmp(s, "e") == 0) return INPUT_EXIT;
-    else {
+    else 
+    {
         printf("Unrecognized command, please try again.\n");
         return process_input_menu();
     }
@@ -70,4 +90,126 @@ int show_help()
 
     scanf("%s", s);
     return show_welcome();
+}
+
+int show_welcome_connected(int socket_desc) 
+{
+    printf("+-----------------------------------------------+\n");
+    printf("|                                               |\n");
+    printf("|  Welcome to Celphi-Zephyr Web Client service  |\n");
+    printf("|                                               |\n");
+    printf("+-----------------------------------------------+\n");
+    printf("|  IP: 127.0.0.1 | Current status:  Connected   |\n");
+    printf("+-----------------------------------------------+\n");
+    printf("|  Options:                                     |\n");
+    printf("|  1. Get hostname : enter \"hostname\" or \"hn\"   |\n");
+    printf("|  2. Get IP :       enter \"hostip\" or \"hi\"     |\n");
+    printf("|  3. Get time :     enter \"time\"               |\n");
+    printf("|  4. Get user list: enter \"list\"               |\n");
+    printf("|  5. Send message : use format \"(id)*\"         |\n");
+    printf("|  6. Disconnect :   enter \"exit\"               |\n");
+    printf("+-----------------------------------------------+\n");
+    return process_input_connected(socket_desc);
+}
+
+void show_nothing(int socket_desc, int out) 
+{
+    receive_reply(socket_desc, STDOUT_FILENO);  
+    io_release();
+}
+
+int check_format(char* s, int* chat_id, int* mesg_start)
+{
+    int p = 0;
+    while (s[p] == ' ') p++;
+    if(s[p++] != '(')
+    {
+        chat_id = NULL;
+        mesg_start = NULL;
+        return NAN;
+    }
+    while (s[p] == ' ') p++;
+    int temp = 0;
+    while(s[p] >='0' && s[p] <= '9')
+    {
+        temp = temp*10 + s[p] - '0';
+        p++;
+    }
+    while (s[p] == ' ') p++;
+    if(s[p++]!= ')')
+    {
+        chat_id = NULL;
+        mesg_start = NULL;
+        return NAN;
+    }
+    *chat_id = temp;
+    *mesg_start = p;
+    return IS_NUM;
+}
+
+int process_input_connected(int socket_desc) 
+{
+    while (io_should_wait());
+    scanf("%s", s);
+    io_locked();
+
+    if(strcmp(s, "hostname")==0 || strcmp(s, "hn") ==0) 
+    {
+#ifdef CLI_TEST
+        printf("requesting hostname\n");
+#endif
+        int ret;
+        do 
+        {
+            ret=request_hostname(socket_desc);
+#ifdef CLI_TEST
+            printf("%d\n", ret);
+#endif
+        } while (ret < 0);
+    }   
+    else if (strcmp(s, "hostip")==0 || strcmp(s, "hi")==0) 
+    {
+        printf("127.0.0.1\n");
+    }
+    else if (strcmp(s, "time") == 0) 
+    {
+#ifdef CLI_TEST
+        printf("requesting time\n");
+#endif
+        int ret;
+        do 
+        {
+            ret = request_time(socket_desc);
+#ifdef CLI_TEST
+            printf("%d\n", ret);
+#endif
+        } while (ret < 0);
+    }
+    else if (strcmp(s, "list")==0) 
+    {
+        int ret;
+        do 
+        {
+            ret = request_listing_clients(socket_desc);
+#ifdef CLI_TEST
+            printf("%d\n", ret);
+#endif
+        } while (ret < 0);
+    }
+    else if (strcmp(s, "exit")==0)
+    {
+        close(socket_desc);
+        return INPUT_EXIT;
+    }
+    else 
+    {
+        int chat_id = 0;
+        int mesg_start;
+        int is_mesg = check_format(s, &chat_id, &mesg_start);
+
+        // TODO: implement sending message
+        printf("Unrecognized input, please try again.\n");
+        io_release();
+    }
+    return process_input_connected(socket_desc);
 }
--- client_cli.h
+++ client_cli.h
@@ -1,16 +1,27 @@
 #ifndef __ZEPHYR_CLIENT_CLI
 #define __ZEPHYR_CLIENT_CLI
 #include "commons.h"
+#include "client_util.h"
 
-#define INPUT_INVALID   0
-#define INPUT_CONNECT   1
-#define INPUT_EXIT      2
+#define INPUT_INVALID   0x000
+#define INPUT_CONNECT   0x010
+#define INPUT_EXIT      0X100
 
+#define NAN             -1
+#define IS_NUM          1
 
 int show_welcome();
 
-int process_input_menu();
+int show_welcome_connected(int socket_desc);
+
+void show_nothing(int socket_desc, int out);
 
 int show_help();
+
+int process_input_menu();
+
+int process_input_connected(int socket_desc);
+
+void io_release();
 
 #endif
