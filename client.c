963c668bac2dc3685a1e34de8e459b12068258df
Zephyr <3150105024@zju.edu.cn>
2018-04-08 16:27:12 +0800

    minor modification

--- client.c
+++ client.c
@@ -1,20 +1,24 @@
 #include "commons.h"
 #include "client_util.h"
 #include "client_cli.h"
-#include <pthread.h>
 
 int client_should_stop=0;
 int socket_desc;
 
-void* start_listen(){
-    printf("listen running\n");
+void* start_listen()
+{
+#ifdef PTHREAD_TEST
+    printf("client listen running\n");
+#endif
     while(!client_should_stop)
     {
         // receive and output section
         // TODO: parse message and implement client status change 
-        receive_reply(socket_desc, STDOUT_FILENO);
+        show_nothing(socket_desc, STDOUT_FILENO);
     }
+#ifdef PTHREAD_TEST
     printf("client receive stop\n");
+#endif
     pthread_exit(0);
 }
 
@@ -37,19 +41,16 @@ int main()
             return 0;
         }
     }
-
     pthread_t pid;
     pthread_create(&pid, NULL, start_listen, NULL);
 
-    while(!client_should_stop) 
+    input_ret = show_welcome_connected(socket_desc);
+    if(input_ret != INPUT_EXIT) {
+        return -1;
+    } else 
     {
-        // Input and send section
-        // TODO: implement client status and operations
-
-        // Welcome Scene:
-        request_hostname(socket_desc);
-        // TODO: synchronize send thread and recv thread
-        client_should_stop = 1;
+        printf("Bye\n");
+        pthread_cancel(pid);        
     }
     pthread_join(pid, NULL);
     // request_send_message(socket_desc, 3, "Hello");
