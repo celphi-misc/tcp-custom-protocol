9c7cb5d54c5b0d9c7a5ed35d1ec89418b2fc1123
ZephyrL <3150104024@zju.edu.cn>
2018-04-10 01:27:09 +0800

    bug fixed: in branch init(); reconstruct interact workflow

--- client_cli.c
+++ client_cli.c
@@ -3,6 +3,8 @@
 
 char *s;
 int io_lock;
+int client_should_stop=0;
+int socket_desc;
 
 void io_locked() 
 {
@@ -26,23 +28,45 @@ void cli_init()
     io_lock = 0;
 }
 
-int show_welcome() 
+void* start_listen()
 {
-    cli_init();
-    printf("+-----------------------------------------------+\n");
-    printf("|                                               |\n");
-    printf("|  Welcome to Celphi-Zephyr Web Client service  |\n");
-    printf("|                                               |\n");
-    printf("+-----------------------------------------------+\n");
-    printf("|  IP: 127.0.0.1 | Current status: Unconnected  |\n");
-    printf("+-----------------------------------------------+\n");
-    printf("|  Options:                                     |\n");
-    printf("|  1. Connect: \tenter \"connect\" or \"c\".         |\n");
-    printf("|  2. Help: \tenter \"help\" or \"h\".            |\n");
-    printf("|  3. Exit: \tenter \"exit\" or \"e\".            |\n");
-    printf("+-----------------------------------------------+\n");
-    return process_input_menu();
+#ifdef PTHREAD_TEST
+    printf("client listen running\n");
+#endif
+    while(!client_should_stop)
+    {
+        show_nothing(socket_desc, STDOUT_FILENO);
+    }
+#ifdef PTHREAD_TEST
+    printf("client receive stop\n");
+#endif
+    pthread_exit(0);
 }
+
+int cli_connect() 
+{   
+    printf("Connecting .. \n");
+    socket_desc = new_socket(8888, "127.0.0.1");
+    if(socket_desc < 0){
+        printf("Socket connect failed, please try again.\n");
+        return process_input_menu();
+    }
+    else 
+    {
+        printf("Connected.\n");
+
+        pthread_t pid;
+        pthread_create(&pid, NULL, start_listen, NULL);
+
+        int ret = show_welcome_connected(socket_desc);
+        if(ret == INPUT_DISCONNECT) {
+            pthread_cancel(pid);
+            return show_welcome();
+        }
+        else 
+            return INPUT_INVALID;
+    }
+}  
 
 int process_input_menu() 
 {
@@ -51,9 +75,17 @@ int process_input_menu()
     printf("%d %d %d\n", strcmp(s, "connect"), strcmp(s, "help"), strcmp(s, "exit"));
 #endif
     
-    if(strcmp(s, "connect") ==0 || strcmp(s, "c")==0) return INPUT_CONNECT;
-    else if(strcmp(s, "help") == 0 || strcmp(s, "h") == 0) return show_help();
-    else if(strcmp(s, "exit") ==0 || strcmp(s, "e") == 0) return INPUT_EXIT;
+    if(strcmp(s, "connect") ==0 || strcmp(s, "c")==0) return cli_connect();  
+    else if(strcmp(s, "help") == 0 || strcmp(s, "h") == 0) 
+    {
+        show_help();
+        return show_welcome();
+    }
+    else if(strcmp(s, "exit") ==0 || strcmp(s, "e") == 0) 
+    {
+        client_should_stop = 1;
+        return INPUT_EXIT;
+    }
     else 
     {
         printf("Unrecognized command, please try again.\n");
@@ -61,7 +93,7 @@ int process_input_menu()
     }
 }
 
-int show_help() 
+void show_help() 
 {
     printf("+-----------------------------------------------+\n");
     printf("|                  Help Manual                  |\n");
@@ -83,13 +115,14 @@ int show_help()
     printf("|    (id)* : format to chat with another client |\n");
     printf("|       id is the number of client, * is your   |\n");
     printf("|       message.                                |\n");
+    printf("|       example:                                |\n");
+    printf("|         (4)Hello! How are you ?               |\n");
     printf("|    exit : to quit or disconnect from server   |\n");
     printf("+-----------------------------------------------+\n"); 
     printf("|  Input anything to return to the upper level  |\n");
     printf("+-----------------------------------------------+\n"); 
 
     scanf("%s", s);
-    return show_welcome();
 }
 
 int show_welcome_connected(int socket_desc) 
@@ -103,7 +136,7 @@ int show_welcome_connected(int socket_desc)
     printf("+-----------------------------------------------+\n");
     printf("|  Options:                                     |\n");
     printf("|  1. Get hostname : enter \"hostname\" or \"hn\"   |\n");
-    printf("|  2. Get IP :       enter \"hostip\" or \"hi\"     |\n");
+    printf("|  2. Get host IP :  enter \"hostip\" or \"hi\"     |\n");
     printf("|  3. Get time :     enter \"time\"               |\n");
     printf("|  4. Get user list: enter \"list\"               |\n");
     printf("|  5. Send message : use format \"(id)*\"         |\n");
@@ -118,7 +151,7 @@ void show_nothing(int socket_desc, int out)
     io_release();
 }
 
-// format : "(num)message"
+// format : "(id)message"
 int check_format(char* s, int* chat_id, int* mesg_start)
 {
     int p = 0;
@@ -155,13 +188,32 @@ int check_format(char* s, int* chat_id, int* mesg_start)
     return IS_NUM;
 }
 
+void cli_input(char* s){
+    char c = ' ';
+    int length = 0;
+    while(1){
+        scanf("%c", &c);
+        if(c=='\n') break;
+        s[length++] = c;
+    }
+    s[length] = 0;
+}
+
 int process_input_connected(int socket_desc) 
 {
     while (io_should_wait());
-    scanf("%s", s);
+    cli_input(s);
     io_locked();
 
-    if(strcmp(s, "hostname")==0 || strcmp(s, "hn") ==0) 
+    if(strcmp(s, "\n" )==0 || strcmp(s, "")==0){
+        io_release();
+    }
+    else if (strcmp(s, "help")==0) {
+        show_help();
+        io_release();
+        return show_welcome_connected(socket_desc);
+    }
+    else if(strcmp(s, "hostname")==0 || strcmp(s, "hn") ==0) 
     {
 #ifdef CLI_TEST
         printf("requesting hostname\n");
@@ -177,8 +229,14 @@ int process_input_connected(int socket_desc)
     }   
     else if (strcmp(s, "hostip")==0 || strcmp(s, "hi")==0) 
     {
-        printf("127.0.0.1\n");
-        io_release();
+        int ret;
+        do 
+        {
+            ret=request_host_ip(socket_desc);
+#ifdef CLI_TEST
+            printf("%d\n", ret);
+#endif
+        } while (ret < 0);
     }
     else if (strcmp(s, "time") == 0) 
     {
@@ -210,16 +268,16 @@ int process_input_connected(int socket_desc)
         request_disconnect(socket_desc);
         close(socket_desc);
         printf("Disconnected.\n");
-        return show_welcome();
+        return INPUT_DISCONNECT;
     }
     else 
     {
         int chat_id = 0;
         int mesg_start;
         int is_mesg = check_format(s, &chat_id, &mesg_start);
-        if(!is_mesg) 
+        if(is_mesg == NAN) 
         {
-            printf("Unrecognized input, please try again.\n");
+            printf("Unrecognized input \"%s\", please try again.\n", s);
             io_release();
         } 
         else 
@@ -228,4 +286,22 @@ int process_input_connected(int socket_desc)
         }
     }
     return process_input_connected(socket_desc);
+}
+
+int show_welcome() 
+{
+    cli_init();
+    printf("+-----------------------------------------------+\n");
+    printf("|                                               |\n");
+    printf("|  Welcome to Celphi-Zephyr Web Client service  |\n");
+    printf("|                                               |\n");
+    printf("+-----------------------------------------------+\n");
+    printf("|  IP: 127.0.0.1 | Current status: Unconnected  |\n");
+    printf("+-----------------------------------------------+\n");
+    printf("|  Options:                                     |\n");
+    printf("|  1. Connect: \tenter \"connect\" or \"c\".         |\n");
+    printf("|  2. Help: \tenter \"help\" or \"h\".            |\n");
+    printf("|  3. Exit: \tenter \"exit\" or \"e\".            |\n");
+    printf("+-----------------------------------------------+\n");
+    return process_input_menu();
 }
