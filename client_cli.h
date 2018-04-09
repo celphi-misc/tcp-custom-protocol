554929ea5363bfd8cdcaac3edc979d20e3958f5d
ZephyrL <3150104024@zju.edu.cn>
2018-04-10 01:27:50 +0800

    reorder enum return values

--- client_cli.h
+++ client_cli.h
@@ -3,9 +3,10 @@
 #include "commons.h"
 #include "client_util.h"
 
-#define INPUT_INVALID   0x000
-#define INPUT_CONNECT   0x010
-#define INPUT_EXIT      0X100
+#define INPUT_INVALID       -1
+#define INPUT_EXIT          0
+#define INPUT_CONNECT       1
+#define INPUT_DISCONNECT    2
 
 #define NAN             -1
 #define IS_NUM          1
@@ -16,12 +17,10 @@ int show_welcome_connected(int socket_desc);
 
 void show_nothing(int socket_desc, int out);
 
-int show_help();
+void show_help();
 
 int process_input_menu();
 
 int process_input_connected(int socket_desc);
-
-void io_release();
 
 #endif
