24fdf8d09eb4864a207573ade0c3c68936a340dc
ZephyrL <3150104024@zju.edu.cn>
2018-04-10 20:01:49 +0800

    add entrance, now allow input exit to exit

--- server.c
+++ server.c
@@ -8,15 +8,17 @@ void* start_serve(void* client_desc)
     pthread_exit(0);
 }
 
-int main(int argc, char **argv)
+void* start_accept(void* server_desc) 
 {
-    int server_desc = new_socket(8888);
-    if(server_desc < 0) return server_desc;
     while (!server_should_stop)
     {
         int client_desc;
-        int accept_exitcode = accept_client(server_desc, &client_desc);
-        if(accept_exitcode != SUCCEED_EXITCODE) return accept_exitcode;
+        int accept_exitcode = accept_client(*(int*)server_desc, &client_desc);
+        if(accept_exitcode != SUCCEED_EXITCODE)
+        {
+            printf("Accept Fault: %d\n", accept_exitcode);
+            pthread_exit(0);
+        }
 
         pthread_t  pid;
         pthread_create(&pid, NULL, start_serve, &client_desc);
@@ -24,5 +26,23 @@ int main(int argc, char **argv)
 
         //service(client_desc);
     }
+    pthread_exit(0);
+}
+
+int main(int argc, char **argv)
+{
+    int server_desc = new_socket(8888);
+    if(server_desc < 0) return server_desc;
+    pthread_t pid;
+    pthread_create(&pid, NULL, start_accept, &server_desc);
+
+    char input[256];
+    scanf("%s", input);
+    if(strcmp((char*)input, (char*)"exit")==0) 
+    {
+        server_should_stop = 1;
+        close(server_desc);
+    }
+
     return 0;
 }
